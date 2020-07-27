d1fb7c1c8b0408c4d69b0b17e30f46065cc390ed
Jacquet Nathan <jacquet.nathan2@gmail.com>
2020-07-27 15:00:53 +0200

    2

---
+++ 3.2.Test2/questions.js
@@ -0,0 +1,57 @@
+let CreationTableauLangages =  () => {
+    let languages = [ 'Html', 'CSS', 'Java', 'PHP' ];
+    return languages;
+
+}
+
+let CreationTableauNombres =  () => {
+    let nombres = [ 0, 1, 2, 3, 4, 5 ];
+    return nombres;
+
+}
+
+let RemplacementElement =  (langages) => {
+     langages.splice(2, 1, 'Javascript');
+     return langages;
+
+}
+
+let AjoutElementLangages =  (langages) => {
+    langages.push('Ruby', 'Python');
+    return langages;
+
+}
+
+let AjoutElementNombres =  (nombres) => {
+    nombres.unshift(-2, -1);
+    return nombres;
+}
+
+let SuppressionPremierElement =  (langages) => {
+    langages.shift();
+    return langages;
+
+}
+
+let SuppressionDernierElement =  (langages) => {
+    langages.pop();
+    return langages;
+}
+
+let ConversionChaineTableau =  (reseaux_sociaux_chaine) => {
+    return reseaux_sociaux_chaine.split(',');
+}
+
+let ConversionTableauChaine =  (langages) => {
+    return langages.join(',');
+
+}
+
+let TriTableau =  (reseaux_sociaux) => {
+    return reseaux_sociaux.sort();
+
+}
+
+let InversionTableau =  (reseaux_sociaux) =>{
+    return reseaux_sociaux.reverse();
+}
